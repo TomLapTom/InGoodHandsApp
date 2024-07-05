c7a08f867b7e8e8e620da92d1e87a5f47fe9a479
TomLapTom <tomaszlapacz@gmail.com>
2024-07-05 18:12:51 +0200

    added home

---
+++ src/components/HomeThreeColumns.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeThreeColumns = () => {
+
+    return (
+        <section>
+            HomeThreeColumns
+        </section>
+    )
+}
+
+export default HomeThreeColumns;
